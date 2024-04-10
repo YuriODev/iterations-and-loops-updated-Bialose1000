3c89b6d76e0de1ad6a63bdd2e5a7d6575db74945
Bialose1000 <bialoseb@gmail.com>
2024-04-10 17:01:01 +0100

    exercises 27-30

--- exercises/exercise_27.py
+++ exercises/exercise_27.py
@@ -1,1 +1,6 @@
-# Your solution to Exercise 27
+def oddNumber(n):
+    return int(2*n - 1)
+
+n = int(input("Enter a number: "))
+pi = [((4/oddNumber(x))*(-1)**(x+1)) for x in range(1,n+1)]
+print(sum(pi))
